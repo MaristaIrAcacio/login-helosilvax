a0b408b4edbf78d752e1a6cb0497ab31df4c1ee9
heloise <heloise.ribeiro@alunoescolassociais.org.br>
2023-07-25 11:21:46 -0300

    alteração no javascript

--- controller.js
+++ controller.js
@@ -9,3 +9,26 @@ function acessar() {
 		console.log(window.location.href);
     }
 }
+/*FUNÇÃO PARA CRIAR A LISTA DE USUÁRIOS */
+/*
+--->Cria o Array para armazenar os usuários
+*/
+var dadosLista = [];
+function salvarUser(){
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        crialista();
+        //console.log(dadosLista);
+    }
+}
+
+function crialista(){
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+    for(let i=0; i <= (dadosLista.length-1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=' '>Editar</button><button class= 'btn btn-danger' onclick=' '>Excluir</button><td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
